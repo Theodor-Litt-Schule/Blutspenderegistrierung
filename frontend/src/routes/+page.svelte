37ce1a9e7686d5c9e006c0561d0bcef13f3da905
AmphoMoronal <amphomoronal@gmail.com>
2025-02-02 12:33:31 +0100

    verifcation code auth

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -7,38 +7,47 @@
 </svelte:head>
 
 <script>
-	process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0'; // TODO DELETE THIS LINE IF YOU GO IN PRODUCTION USE!!!
+	// process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0'; // TODO DELETE THIS LINE IF YOU GO IN PRODUCTION USE!!!
+	//
+	// async function login(event) {
+	// 	console.log('in');
+	// 	event.preventDefault(); // prevent the default behave of the form
+	//
+	// 	const nameInput = event.target.querySelector('#input-name');
+	// 	const mailInput = event.target.querySelector('#input-mail');
+	//
+	// 	const name = nameInput.value;
+	// 	const mail = mailInput.value;
+	//
+	// 	let data = {
+	// 		name: name,
+	// 		email: mail
+	// 	};
+	// 	// TODO finish the Login here
+	// 	try {
+	// 		await fetch('https://localhost:5000/login', {
+	// 			method: 'GET',
+	// 			cache: 'no-cache',
+	// 			credentials: 'same-origin',
+	// 			headers: new Headers({
+	// 				'content-type': 'application/json'
+	// 			}),
+	// 			data: JSON.stringify(data)
+	// 		});
+	// 	} catch (error) {
+	// 		console.error('Fetch error:', error);
+	// 	}
+	// }
+	import {goto} from "$app/navigation";
+    import { redirect } from "@sveltejs/kit";
 
-	async function login(event) {
-		console.log('in');
-		event.preventDefault(); // prevent the default behave of the form
 
-		const nameInput = event.target.querySelector('#input-name');
-		const mailInput = event.target.querySelector('#input-mail');
+	let email = '';
 
-		const name = nameInput.value;
-		const mail = mailInput.value;
-
-		let data = {
-			name: name,
-			email: mail
-		};
-		// TODO finish the Login here
-		try {
-			await fetch('https://localhost:5000/login', {
-				method: 'GET',
-				cache: 'no-cache',
-				credentials: 'same-origin',
-				headers: new Headers({
-					'content-type': 'application/json'
-				}),
-				data: JSON.stringify(data)
-			});
-		} catch (error) {
-			console.error('Fetch error:', error);
-		}
+	function handleSubmit() {
+		document.cookie = `email=${email}`;
+		redirect(308, `/processing`);
 	}
-
 	// No Navigator needed, because the API redirects you to the questions page
 </script>
 
@@ -46,16 +55,28 @@
 	<form class='login-form'>
 		<h1>Anmeldung</h1>
 
-		<img alt='IServ-Logo' class='iserv-logo' src='img/ISERV.svg'>
+<!--		<img alt='IServ-Logo' class='iserv-logo' src='img/ISERV.svg'>-->
 
-		<a class='btn-style1' href='https://blutspenden.novacodes.eu/api/iservlogin'>Daten über IServ importieren</a>
+		<p>Bitte geben Sie Ihre IServ E-Mail-Adresse ein, um sich für die Blutspende anzumelden.</p>
 
-		<br>
+		<form on:submit={handleSubmit}>
+        <input
+            type="email"
+            bind:value={email}
+            placeholder="vorname.nachname@tls-giessen.eu"
+			pattern="[A-Za-z0-9.]+@tls-giessen.eu"
+            required
+        />
+		<br><br>
+        <button id='dynamic' class='btn-style1' type="submit">Submit</button>
+    	</form>
 
 		<hr>
 
 		<p>Mit dem Fortfahren des Registrierungsprozesses stimmen Sie zu, dass wir Ihre Daten bis zu dem Spendetermin
 			speichern.</p>
+
+
 
 		<a href='http://wp.tls-gi.de/impressum/'>Impressum</a>
 	</form>
--- frontend/src/routes/processing/+page.svelte
+++ frontend/src/routes/processing/+page.svelte
@@ -5,10 +5,14 @@
 <script>
     import { onMount } from "svelte";
     import { goto } from '$app/navigation';
+	import { env } from '$env/dynamic/public';
+	import {redirect} from "@sveltejs/kit";
+
 
     onMount( () => {
         document.cookie = `user_id=${window.location.search.split('=')[1]}`;
-				goto("/questions")
+		let api = env.API_DOMAIN;
+		redirect(300, `https://127.0.0.1:5000/login`);
     });
 
 </script>
